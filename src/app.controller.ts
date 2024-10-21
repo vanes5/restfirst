b7e957f1ef1744b6f8672981fd36502ddfda66c1
Gecse Vanessza <vanessza.gecse@gmail.com>
2024-10-21 13:09:30 +0200

    elso

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,5 +11,27 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  #products =[
+    'Bucket',
+    'REST API for pros',
+    'Tablet'
+  ]
+
+
+  @Get('products')
+  listProducts(){
+    return this.#products;
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id:string){
+    return this.#products[Number(id)];
+  }
+
+  @Delete('products/:id')
+  deleteProduct(@Param('id') id:string){
+    this.#products.splice(Number(id), 1);
   }
 }
